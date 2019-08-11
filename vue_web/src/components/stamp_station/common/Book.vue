c87982dd86eeaa445bdc4a08212ba5e00d9007b5
fointo <2353036514@qq.com>
2019-08-11 18:05:13 +0800

    to

---
+++ vue_web/src/components/stamp_station/common/Book.vue
@@ -0,0 +1,29 @@
+<template>
+    <div dir="books">
+        <!-- 顶部渐变色 -->
+        <div class="book-top">
+            <img src="http://www.51gu.com/cn/images/title_h1.gif">
+            <strong>电子目录</strong>
+        </div>
+        <!-- 商品图片 循环 -->
+		<div class="book-item">
+            <a href="javascript:;">
+                <img src="http://www.51gu.com/UploadFiles/image/20171213021828944.jpg" border="0" width="106" height="150">
+            </a>
+            <a href="javascript:;">2017年第5期</a>
+		</div>
+        <!-- 提示语 -->
+        <p>注：电子目录内的邮品价格均为出刊时的价格，部分邮品价格可能会因一些原因而变动，请以“网上超市”内的价格为准！</p>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{}
+    }
+}
+</script>
+<style>
+
+</style>
+
---
+++ vue_web/src/components/stamp_station/common/BookNext.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="book-next">
+        <!-- 顶部渐变色 引用组件 -->
+        <!-- 二级页面内容 -->
+        <div class="next-top">
+            <p><strong>2017年第5期</strong></p>
+            <a href="http://www.51gu.com/UploadFiles/file/201712131429386675727-p1-p11.pdf">1、特别推荐、中国事物、欧罗巴、邮政、医学、文化艺术、军事专栏</a>
+            <a href="http://www.51gu.com/UploadFiles/file/201712131427208550727-p12-p17.pdf">2、政治组织、交通运输、动植物、旅游风光专栏</a>
+            <a href="http://www.51gu.com/UploadFiles/file/201712131425495425727-p18-p49.pdf">3、邮票列国志—马尔代夫、朝鲜</a>
+            <a href="http://www.51gu.com/UploadFiles/file/201712131422312300727-p50-p65.pdf">4、邮票列国志—朝鲜、体育、人物专栏</a>
+            <a href="javascript:;">返回</a>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{}
+    }
+}
+</script>
+<style>
+
+</style>
+
+
