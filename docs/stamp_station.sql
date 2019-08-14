26f94fe797170af4315746d6b81d540c719e2478
19xxhh <2385218695.@qq.com>
2019-08-14 19:39:00 +0800

    修改数据库

--- docs/stamp_station.sql
+++ docs/stamp_station.sql
@@ -16,7 +16,7 @@ create table stamp_details(
  imgurl VARCHAR(256) comment "图片地址",
  detials VARCHAR(256) comment "详细介绍", 
  kid VARCHAR(128) comment " 种类ID，关联表：kinds",
- kname VARCHAR(300) comment " 种类名称，关联表：kinds",
+ kname VARCHAR(300) comment " 种类名称，关联表：kinds"
  --subid INT comment " 专题ID，关联表：subjects",
  --sub_name VARCHAR(300) comment " 专题名称，关联表：subjects",
 );
