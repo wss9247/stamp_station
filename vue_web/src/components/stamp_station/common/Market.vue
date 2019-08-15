bbc9ffbe62ff6408beb6bcd835488e49f38d3d5f
19xxhh <2385218695.@qq.com>
2019-08-15 17:35:48 +0800

    更新market.vue

--- vue_web/src/components/stamp_station/common/Market.vue
+++ vue_web/src/components/stamp_station/common/Market.vue
@@ -1,27 +1,30 @@
 <template>
-	
 	<div id="market">
 		<!-- 下拉选择框 -->
-		<h3  id="choice">
+		<h3  class="choice">
 			<select>
 				<option>请选择排列规则</option>
 			</select>
 		</h3>
 		<div class="probox">
 		<!-- 商品 -->
-			<div id="product" v-for="(prop,i) of pros" :key="i">
-				<a @click="jumpTo"><img class="product-img" :src="prop.imgurl"></a>			
+			<div class="product" v-for="(prop,i) of pros" :key="i">
+				<div @click="jumpTo" class="product-img">
+					<a :style="`background-image:url('${prop.imgurl}');`"></a>
+				</div>			
 				<ul>
 					<li>编号：{{prop.snum}}</li>
 					<li>国家：{{prop.nname}}</li>
-					<li>发行年份：{{prop.sdata}}</li>
+					<li>发行年份：{{prop.sdate}}</li>
 					<li>内容：{{prop.stitle}}</li>
 					<li>价格：{{prop.price}}</li>
 				</ul>
-				<a href="javascript:;"><img src="http://www.51gu.com/cn/images/but_dg.jpg">
-				</a>
-				<a href="javascript:;"><img src="http://www.51gu.com/cn/images/but_che.jpg">
-				</a>
+				<div class="collection">
+					<a href="javascript:;"><img src="http://www.51gu.com/cn/images/but_dg.jpg">
+					</a>
+					<a href="javascript:;"><img src="http://www.51gu.com/cn/images/but_che.jpg">
+					</a>
+				</div>
 			</div>
 		</div>
   </div>
@@ -32,18 +35,28 @@ export default {
 		pros:[]
 	}},
 	methods:{
-		jumpTo(e){ 
-			
+		jumpTo(e){ 	
       		this.$router.push("/info");// 点击后跳转到宝贝详情页
-      
 		},
-	
 	},
 	created(){
+		// 获取后台数据
 		this.axios.get("market").then(res=>{
 			this.pros=res.data.data;
-			console.log(res)
+			for(var pro of this.pros){
+				var date=pro.sdate;
+				var sdate=new Date(date);
+				var year=sdate.getUTCFullYear();
+				var month=sdate.getMonth()+1;
+				var date1=sdate.getDate();
+				pro.sdate=year+"-"+month+"-"+date1
+			}
+			
+			console.log(year)
 		})
+		// var date=new Date(this.pros[0]);
+		// console.log(date)
+		// date=getFullYear(date);
 	},
 }
 </script>
